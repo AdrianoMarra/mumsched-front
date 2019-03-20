ef16f2e87f9ed2c2aef89387966f89549b6ce847
Adriano Marra <adrianomarra90@gmail.com>
2019-03-19 23:11:04 -0500

    Update the code with the new features for faculty and student areas.

--- src/js/controllers/faculty/faculty.js
+++ src/js/controllers/faculty/faculty.js
@@ -7,10 +7,71 @@ angular
     $state.go('appSimple.loginFaculty')
   }
 
-
   $scope.logout = function() {
     localStorage.removeItem("facultyData");
     $state.go('appSimple.home')
+  }
+
+  $scope.getBlockPreferences = function() {
+      // $http.get('http://172.19.143.87:8000/api/faculty/'+$scope.facultyData.id+'/blockpreferences',
+      // // $httpParamSerializer(userInfo),
+      // {headers: {'Content-Type': 'application/json'}})
+      // .then(function successCallback(response) {
+
+      //     $scope.courses = response.data.courses;
+      //     console.log($scope.courses);
+
+      // }, function errorCallback(response) {
+      //     console.log(response);
+      // });
+  };
+
+  $scope.submitBlockPreferences = function() {
+
+  	var blockIdsPreferences = [];
+  	for (var i = 0; i < $scope.blocks.length; i++) {
+  		if ($scope.blocks[i].preference) {
+  			blockIdsPreferences.push($scope.blocks[i].block_id);
+  		}
+  		console.log(blockIdsPreferences);
+  	}
+  }
+
+  $scope.getCoursePreferences = function() {
+      $http.get('http://172.19.143.87:8000/api/faculty/'+$scope.facultyData.id+'/coursepreferences',
+      // $httpParamSerializer(userInfo),
+      {headers: {'Content-Type': 'application/json'}})
+      .then(function successCallback(response) {
+
+          $scope.courses = response.data.courses;
+          console.log($scope.courses);
+
+      }, function errorCallback(response) {
+          console.log(response);
+      });
+  }
+
+  $scope.submitCoursePreferences = function() {
+    var courseIdsPreferences = [];
+    for (var i = 0; i < $scope.courses.length; i++) {
+      if ($scope.courses[i].isPreference) {
+        courseIdsPreferences.push($scope.courses[i].id);
+      }
+    }
+
+    var params = {"courses_id": courseIdsPreferences};
+
+    $http.put('http://172.19.143.87:8000/api/faculty/'+$scope.facultyData.id+'/coursepreferences',
+    // $httpParamSerializer(userInfo),
+    params,
+    {headers: {'Content-Type': 'application/json'}})
+    .then(function successCallback(response) {
+        console.log(response);
+        alert("Thank you, preference list updated successfully! ");
+
+    }, function errorCallback(response) {
+        console.log(response);
+    });
   }
 
 }]);
