74c56baa8094dc9fa22b2a20f165f3b4f2632880
Adriano Marra <adrianomarra90@gmail.com>
2019-03-20 11:12:30 -0500

    Creating the block preference for the faculty

--- src/js/controllers/faculty/faculty.js
+++ src/js/controllers/faculty/faculty.js
@@ -13,28 +13,38 @@ angular
   }
 
   $scope.getBlockPreferences = function() {
-      // $http.get('http://172.19.143.87:8000/api/faculty/'+$scope.facultyData.id+'/blockpreferences',
-      // // $httpParamSerializer(userInfo),
-      // {headers: {'Content-Type': 'application/json'}})
-      // .then(function successCallback(response) {
+      $http.get('http://172.19.143.87:8000/api/faculty/'+$scope.facultyData.id+'/blockpreferences',
+      // $httpParamSerializer(userInfo),
+      {headers: {'Content-Type': 'application/json'}})
+      .then(function successCallback(response) {
 
-      //     $scope.courses = response.data.courses;
-      //     console.log($scope.courses);
+          $scope.blocks = response.data.blocks;
+          console.log($scope.blocks);
 
-      // }, function errorCallback(response) {
-      //     console.log(response);
-      // });
+      }, function errorCallback(response) {
+          console.log(response);
+      });
   };
 
   $scope.submitBlockPreferences = function() {
+    var blockIdsPreferences = [];
+    for (var i = 0; i < $scope.blocks.length; i++) {
+      if ($scope.blocks[i].isPreference) {
+        blockIdsPreferences.push($scope.blocks[i].id);
+      }
+    }
 
-  	var blockIdsPreferences = [];
-  	for (var i = 0; i < $scope.blocks.length; i++) {
-  		if ($scope.blocks[i].preference) {
-  			blockIdsPreferences.push($scope.blocks[i].block_id);
-  		}
-  		console.log(blockIdsPreferences);
-  	}
+    var params = {"blocks_id": blockIdsPreferences};
+
+    $http.put('http://172.19.143.87:8000/api/faculty/'+$scope.facultyData.id+'/blockpreferences',
+    // $httpParamSerializer(userInfo),
+    params,
+    {headers: {'Content-Type': 'application/json'}})
+    .then(function successCallback(response) {
+        alert("Thank you, preference list updated successfully! ");
+    }, function errorCallback(response) {
+        console.log(response);
+    });
   }
 
   $scope.getCoursePreferences = function() {
@@ -44,7 +54,6 @@ angular
       .then(function successCallback(response) {
 
           $scope.courses = response.data.courses;
-          console.log($scope.courses);
 
       }, function errorCallback(response) {
           console.log(response);
@@ -66,9 +75,7 @@ angular
     params,
     {headers: {'Content-Type': 'application/json'}})
     .then(function successCallback(response) {
-        console.log(response);
         alert("Thank you, preference list updated successfully! ");
-
     }, function errorCallback(response) {
         console.log(response);
     });
